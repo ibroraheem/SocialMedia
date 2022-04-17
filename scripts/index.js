40498686fcfaf60c1c5bb85c8ec5ad10791df20b
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2022-04-17 11:51:33 +0100

    CreatePost

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,29 @@
 // Javascript
+let form = document.getElementById("form");
+let input = document.getElementById("input").value;
+let posts = document.getElementById("posts");
+let post = {};
+let postList = document.getElementById("post-list");
+post['text'] = input;
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("button clicked");
+
+    if (input.value === "") {
+        alert("Post Cannot be Empty!")
+
+    } else {
+        console.log(post.text);
+        createPost();
+    }
+});
+
+function createPost() {
+    postList.innerHTML += `<li>
+    ${post.text}
+    <span
+      ><i class="fas fa-pen"></i> <i class="fas fa-trash"></i
+    ></span>
+  </li>`;
+    input = "";
+}
