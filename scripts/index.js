3f1d533efd391ebd54379d5fbf22b6442084f59d
ibnyahyah <yahyahridwan665@gmail.com>
2022-04-18 16:16:49 +0100

    clearing input field after submittion

--- scripts/index.js
+++ scripts/index.js
@@ -2,26 +2,23 @@
 let form = document.getElementById("form");
 var input = document.getElementById("input");
 let posts = document.getElementById("posts");
-var postArray = []
+var postArray = [];
 let postList = document.getElementById("post-list");
 
-
-
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    if (input === "") {
-        alert("Please enter a post");
-    } else {
-        var inputValue = input.value
-        postArray.push(input.value);
-        inputValue = "";
+  event.preventDefault();
+  if (input === "") {
+    alert("Please enter a post");
+  } else {
+    var inputValue = input.value;
+    postArray.push(input.value);
+    inputValue = "";
 
-        postList.innerHTML += ` <li>
-        ${postArray[postArray.length - 1]}<span><i class="fas fa-pen"></i> <i class="fas fa-trash"></i></span>
-      </li>`
-    }
+    postList.innerHTML += ` <li>
+        ${
+          postArray[postArray.length - 1]
+        }<span><i class="fas fa-pen"></i> <i class="fas fa-trash"></i></span>
+      </li>`;
+    input.value = "";
+  }
 });
-
-
-
-
